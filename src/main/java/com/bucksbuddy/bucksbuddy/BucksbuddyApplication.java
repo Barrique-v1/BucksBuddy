273961890cfc2d131582f5a00c5370b2336226f2
Norman Peters <peters.norman91@gmail.com>
2024-04-10 17:18:44 +0200

    Initial commit

---
+++ src/main/java/com/bucksbuddy/bucksbuddy/BucksbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.bucksbuddy.bucksbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucksbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BucksbuddyApplication.class, args);
+	}
+
+}
