0aacabac1d66d2ff993e473acb21d821089f5a48
maratsch <s0589160@htw-berlin.de>
2024-05-15 13:41:44 +0200

    added h2 database connection

--- src/main/java/com/bucksbuddy/bucksbuddy/Expenditures.java
+++ src/main/java/com/bucksbuddy/bucksbuddy/Expenditures.java
@@ -1,15 +1,24 @@
 package com.bucksbuddy.bucksbuddy;
 
-public class Expenditures {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "expenditures")
+public class Expenditures {
+    @Id
+    private int id;
     private String name;
     private double amount;
     private String person;
+    private String currency;
 
-    public Expenditures(String name, double amount, String person) {
-    this.name = name;
-    this.amount = amount;
-    this.person = person;
+    public Expenditures(int id, String name, double amount, String person, String currency) {
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+        this.person = person;
+        this.currency = currency;
+
     }
 
     public String getName() {
--- src/main/java/com/bucksbuddy/bucksbuddy/HelloController.java
+++ src/main/java/com/bucksbuddy/bucksbuddy/HelloController.java
@@ -16,10 +16,10 @@ public class HelloController {
 
     @GetMapping("/expenses")
     public List<Expenditures> getExpenses() {
-        Expenditures expenditure1 = new Expenditures("Kartoffeln", 12.02, "Peter");
-        Expenditures expenditure2 = new Expenditures("Kartoffeln", 12.02, "Peter");
-        Expenditures expenditure3 = new Expenditures("Kartoffeln", 12.02, "Peter");
-        Expenditures expenditure4 = new Expenditures("Kartoffeln", 12.02, "Peter");
+        Expenditures expenditure1 = new Expenditures( 1,"Kartoffeln", 12.02, "Peter", "EUR");
+        Expenditures expenditure2 = new Expenditures(2,"Kartoffeln", 12.02, "Peter", "EUR");
+        Expenditures expenditure3 = new Expenditures(3,"Kartoffeln", 12.02, "Peter", "EUR");
+        Expenditures expenditure4 = new Expenditures(4,"Kartoffeln", 12.02, "Peter", "EUR");
         return List.of(expenditure1,expenditure2,expenditure3,expenditure4);
     }
 }
