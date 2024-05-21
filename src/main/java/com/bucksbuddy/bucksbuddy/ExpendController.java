ed75eda00bb44b01b8b5d2f65ffbded926f359bd
Norman <peters.norman91@gmail.com>
2024-05-21 11:42:32 +0200

    get, post, delete, put expense added

--- src/main/java/com/bucksbuddy/bucksbuddy/ExpendController.java
+++ src/main/java/com/bucksbuddy/bucksbuddy/ExpendController.java
@@ -23,11 +23,39 @@ public class ExpendController {
         }
         return new ResponseEntity<>(expenditureInDB.get(), HttpStatus.OK);
     }
+    // get all expenses
+    @GetMapping("/expenses")
+    public ResponseEntity<Iterable<Expenditure>> getExpenditures() {
+        Iterable<Expenditure> expenditures = repo.findAll();
+        return new ResponseEntity<>(expenditures, HttpStatus.OK);
+    }
 
     // create expense
     @PostMapping("/expense")
     public ResponseEntity<Expenditure> createExpenditure(@RequestBody Expenditure newExpenditure){
         repo.save(newExpenditure);
         return new ResponseEntity<>(newExpenditure, HttpStatus.OK);
+    }
+
+    // delete expense by id
+    @DeleteMapping("/expense")
+    public ResponseEntity deleteExpenditure(@RequestParam(value = "id") int id) {
+        Optional<Expenditure> expenditureInDB = repo.findById(id);
+        if (!expenditureInDB.isPresent()) {
+            return new ResponseEntity("No expenditure found with id " + id, HttpStatus.NOT_FOUND);
+        }
+        repo.delete(expenditureInDB.get());
+        return new ResponseEntity("Expenditure with id " + id + " deleted", HttpStatus.OK);
+    }
+
+    // update expense
+    @PutMapping("/expense")
+    public ResponseEntity<Expenditure> updateExpenditure(@RequestBody Expenditure updatedExpenditure) {
+        Optional<Expenditure> expenditureInDB = repo.findById(updatedExpenditure.getId());
+        if (!expenditureInDB.isPresent()) {
+            return new ResponseEntity("No expenditure found with id " + updatedExpenditure.getId(), HttpStatus.NOT_FOUND);
+        }
+        repo.save(updatedExpenditure);
+        return new ResponseEntity<>(updatedExpenditure, HttpStatus.OK);
     }
 }
