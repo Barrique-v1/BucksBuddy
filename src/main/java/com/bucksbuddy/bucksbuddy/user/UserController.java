fdd78d0b42a55c2b670f2c84f3672cb95f907298
Norman <peters.norman91@gmail.com>
2024-05-28 12:12:14 +0200

    work on user

--- src/main/java/com/bucksbuddy/bucksbuddy/user/UserController.java
+++ src/main/java/com/bucksbuddy/bucksbuddy/user/UserController.java
@@ -1,4 +1,15 @@
 package com.bucksbuddy.bucksbuddy.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
 public class UserController {
+
+    @Autowired
+    UserRepository repository;
+
+    // get all users
+
 }
